706864b69be986ee8ebebaad1d0d39c446d0de4b
Loch <lochanapabasara9@gmail.com>
2022-04-30 21:35:21 +0530

    Dashboard Creating

--- System.c
+++ System.c
@@ -25,16 +25,73 @@ void genaratebillfotter ( int dis, int total){
     printf("\nDiscount\t\t\t%d%%", dis);
     printf("\nDiscount Amount\t\t%d", (total/100)*dis);
     printf("\nTotal Amount\t\t%d", total-((total/100)*dis));
-        printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 }
 
 int main()
 {
+    int pw;
+    int opt;
+    char cn[50];
+    int cq;
+    printf("Enter pincode :");
+    
+    scanf("%d",&pw);
 
-int a;
-genaratebillheader ("abc", "hsja");
-genaratebillbody ( 5,"aaaa", 567);
-genaratebillfotter ( 5, 500);
-return(0);
+    if (pw==1234){
+        printf("\tBilling System by Loch");
+        printf("\n\t ===================");
+        printf("\n\n");
+        printf("You are successfully login to the system");
+        printf("\n\n");
+        printf(" Please select your preferred operation");
+        printf("\n\n");
+        printf("1.New bill");
+        printf("\n2.All bills");
+        printf("\n3.Search bills");
+        printf("\n4.Log out");
+        printf("\n\n");
+        printf("Your choice: ");
+        scanf("%d",&opt);
 
-}
+        switch (opt)
+        {
+        case 1:
+
+            system("clear");
+            printf("\tBilling System by Loch");
+            printf("\n\t ===================");
+            printf("\n\n");
+            printf("~~~~Add new bill~~~~");
+            printf("\n\n");
+            printf(" Please enter cusmoter name: ");
+            scanf("%s",&cn);
+            printf("n/ Please enter Number Of items to enter ");
+            scanf("%d",&cq);
+            
+
+            break;
+        
+        case 2:
+            printf("Your choice: 2 ");
+            break;
+        
+        case 3:
+            printf("Your choice: 3 ");
+            break;
+        
+        case 4:
+            printf("Your choice: 4 ");
+            break;
+        
+        default:
+            break;
+        }
+
+    }
+    else {
+        printf("Your pincode is wrong. Please try again");
+    }
+
+    return(0);}
+
