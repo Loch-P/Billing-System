a370dd1bea4559f18dec47b31a20020308f81353
Loch <lochanapabasara9@gmail.com>
2022-04-30 17:57:56 +0530

    Bill body added

--- System.c
+++ System.c
@@ -14,13 +14,18 @@ void genaratebillheader (char name[50], char date[30]){
     printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 }
 
+void genaratebillbody ( int qty,char item[50], int price){
+    printf("\n%d", qty);
+    printf("\t\t%s",item);
+    printf("\t\t%d", price);
+}
 
 int main()
 {
 
 int a;
-
 genaratebillheader ("abc", "hsja");
-
+genaratebillbody ( 5,"aaaa", 567);
 return(0);
+
 }
